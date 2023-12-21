0cf4002cf0378852389ff5cf8a5bd9626d6cc60c
DuskXD <130565144+DuskXD@users.noreply.github.com>
2023-12-21 16:00:52 +0700

    Update Flask.py

--- Flask.py
+++ Flask.py
@@ -1,15 +1,7 @@
-from flask import Flask
+from flask import Flask, render_template
 from flask import request
-import psycopg2
 import requests
-# conn = psycopg2.connect(
-#     dbname = "ppbot",
-#     user = "postgres",
-#     password = "DaS3VLQZ@IOp",
-#     host = "127.0.0.1",
-#     port = "5432"
-#
-# )
+import json
 
 
 app = Flask(__name__)
@@ -27,29 +19,46 @@ params = {
 }
 
 
-@app.route('/auth', methods=['GET'])
+@app.route('/')
+def start():
+    return "Hi"
+
+
+@app.route('/auth', methods=['GET', 'POST'])
 def auth():
     code = request.args.get('code')
     if code:
         auth_headers = {
-            'client_id': client_id,
-            'client_secret': client_secret,
+            'client_id': 'fce7f1d5-c662-4f8a-bf44-0915e81687e9',
+            'client_secret': 'kFpy0P3obIC6UPuafXC664gL41zo2P7n',
             'grant_type': 'authorization_code',
             'code': code
-
         }
-        access_token_response = requests.post(leader_id_api_url_auth, headers=auth_headers)
+        # return data
+        try:
+            access_token_response = requests.post(leader_id_api_url_auth, data=auth_headers)
+        except Exception:
+            return 'Ошибка в получении access token'
         if access_token_response.status_code == 200:
             access_token = access_token_response.json()['access_token']
             refresh_token = access_token_response.json()['refresh_token']
             user_id = access_token_response.json()['user_id']
-            #добавить пользователя в бд с аксесс и рефреш токенами
-            # events_headers = {
-            #
-            #     'Authorization': f'Bearer {access_token}'
-            # }
-            # events_response = request.get(leader_id_api_url_auth, headers = events_headers)
-            return f'Аутентификация прошла успешно. Access token: {access_token}, refresh_token: {refresh_token}, user_id: {user_id}'
+            events_headers = {
+
+                'Authorization': f'Bearer {access_token}'
+            }
+            try:
+                events_response = request.get(leader_id_api_url_auth, headers = events_headers)
+            except Exception:
+                return 'Ошибка в получении мероприятий'
+            if events_response.status_code == 200:
+                events = events_response.json()
+                event_list = [event['name'] for event in events]
+                return f"Мероприятия:\n\n{', '.join(event_list)}"
+            else:
+                return "Не удалось получить список мероприятий"
+            # Далее можно использовать access_token для работы с Leader-ID API
+            return f'Аутентификация прошла успешно, code: {code}. Access token: {access_token}, refresh_token: {refresh_token}, user_id: {user_id}'
         else:
             return 'Ошибка аутентификации'
     else:
